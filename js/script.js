0878bcd64481f2307bcd000dc724e8f7069bf985
cameronmhall <c.hall2116@gmail.com>
2020-11-08 14:31:47 +0800

    Added comments

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+// array of objects with properties quote, source, citation and year
 const quotes = [
   {
     quote: "The best way to get started is to quit talking and begin doing.", 
@@ -38,28 +39,34 @@ const quotes = [
 
 /***
  * `getRandomQuote` function
+ * @return: a randomly selected object from the quotes array
 ***/
-
 function getRandomQuote() {
+  //finds a random number between 0 and 4 inclusive
   let randomNumber = Math.floor(Math.random() * 5);
+  //returns quotes object at index of randomNumber
   return quotes[randomNumber];
 }
 
 /***
  * `printQuote` function
 ***/
-
 function printQuote() {
+  //calls function getRandomQuote and assigns it to a variable
   let randomObject = getRandomQuote();
+  //initialises html string
   let html = "<p class='quote'>" + randomObject.quote + 
-  "</p><p class='source'>" + "randomObject.source";
+  "</p><p class='source'>" + randomObject.source;
+  //if the object has a citation property concatenate to html string
   if (randomObject.hasOwnProperty("citation")) {
-    html += "<span class='citation'> ${randomObject.citation} </span>";
+    html += "<span class='citation'>" + randomObject.citation + "</span>";
   };
+  //if the object has a year property concatenate to html string
   if (randomObject.hasOwnProperty("year")) {
-    html += "<span class='year'> ${randomObject.year} </span>";
+    html += "<span class='year'>" + randomObject.year + "</span>";
   };
   html += "</p>"
+  //display html string in browser
   document.getElementById('quote-box').innerHTML = html; 
 }
 
